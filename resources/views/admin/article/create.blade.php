8e91b56af96ef5510f304b6ef286899c2b6d8db6
shiping <z388110@gmail.com>
2016-10-25 22:17:58 +0800

    edit message add input address

--- resources/views/admin/article/create.blade.php
+++ resources/views/admin/article/create.blade.php
@@ -5,7 +5,7 @@
         <div class="row">
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
-                    <div class="panel-heading">新增一篇文章</div>
+                    <div class="panel-heading">新增留言</div>
                     <div class="panel-body">
 
                         @if (count($errors) > 0)
@@ -17,9 +17,11 @@
 
                         <form action="{{ url('admin/article') }}" method="POST">
                             {!! csrf_field() !!}
-                            <input type="text" name="title" class="form-control" required="required" placeholder="请输入标题">
+                            <input type="text" name="title" class="form-control" required="required" placeholder="請輸入標題">
                             <br>
-                            <textarea name="body" rows="10" class="form-control" required="required" placeholder="请输入内容"></textarea>
+                            <input type="text" name="address" class="form-control" required="required" placeholder="請輸入地址">
+                            <br>
+                            <textarea name="body" rows="10" class="form-control" required="required" placeholder="請輸入內容"></textarea>
                             <br>
                             <button class="btn btn-lg btn-info">新增文章</button>
                         </form>
