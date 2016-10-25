5548ab08dbf2ccf95713bd3d996fc522af6abbf3
shiping <z388110@gmail.com>
2016-10-25 22:13:06 +0800

    map

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <title>Laravel</title>
 
     <link href="//cdn.bootcss.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet">
-    <script src="//cdn.bootcss.com/jquery/1.11.1/jquery.min.js"></script>
+    <script src="//cdn.bootcss.com/jquery/1.11.3/jquery.min.js"></script>
     <script src="//cdn.bootcss.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
 </head>
 <body id="app-layout">
@@ -27,7 +27,7 @@
 
             <!-- Branding Image -->
             <a class="navbar-brand" href="{{ url('/') }}">
-                Laravel
+                LoveMap
             </a>
         </div>
 
--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -1,5 +1,24 @@
 @extends('layouts.app')
 
 @section('content')
- 
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="//cdn.bootcss.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+    <center>
+    <div id="map" style="width: 800px; height: 600px; border:double #dd1144"></div> <!--此為地圖顯示大小-->
+    <script async defer
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyA8hRHdZEOVwpfzjh_Yo5Pu0Aw_RrsOsT8&callback=initMap">
+    </script>
+    </center>
+    <script type="text/javascript">
+        function initMap() {
+            var geocoder = new google.maps.Geocoder();
+            var latlng = new google.maps.LatLng(23.795201261829853, 120.9694504737854);
+            var map = new google.maps.Map(document.getElementById('map'), {
+                center: latlng,
+                zoom: 8
+            });
+
+        }
+
+    </script>
 @endsection
